83b155118d2acd36d94346fa6e0d2078fd1ed7f0
ItzYanick <me@yanick.gay>
2022-06-22 18:44:28 +0200

    database fix

--- database/database.go
+++ database/database.go
@@ -20,7 +20,7 @@ func InitDB() error {
 	}
 
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
 	}
